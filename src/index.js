e26f9027d5af4c734f455f54b7873cbe3d4049ab
Vinicius Benso <vinibenso@gmail.com>
2022-08-03 21:49:10 -0300

    post request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const express = require('express');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+app.use(express.json());
+
+const users = []
+
+app.post('/registration', (request, response)=>{
+  const {name, email, cpf, latitude, longitude, type, date, event} = request.body;
+
+  const userAlredyExists = users.some((user) => user.cpf === cpf);
+  if(userAlredyExists) {
+    return response.status(400).json({error: "Usuário ja cadastrado"})
+  }
+
+  const id = uuidv4();
+
+  users.push({
+    id: uuidv4(),
+    name, 
+    email,
+    cpf, 
+    latitude, 
+    longitude,
+    type, 
+    date, 
+    event
+  });
+
+  return response.status(201).send();
+
+})
 
 app.listen(3333)
